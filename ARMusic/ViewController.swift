6ca7d6ed24ea317c61b91039f977d1dd2cd4773e
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-10-26 18:40:42 -0400

    Initial Commit

---
+++ ARMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARMusic
+//
+//  Created by Valerian Clerc on 10/26/19.
+//  Copyright © 2019 Valerian Clerc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
